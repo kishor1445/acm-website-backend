0fa023d4360843b2ddad1df59e31c23c0810e496
Kishor Ramanan <kishorramanan5@gmail.com>
2023-12-22 20:56:20 +0530

    + checks for empty required data

--- utils/others.py
+++ utils/others.py
@@ -1,11 +1,23 @@
-def get_dict(data, description):
+from typing import List
+
+
+def get_dict(data, description) -> List[dict]:
     title = [x[0] for x in description]
     return [dict(zip(title, x)) for x in data]
 
 
-def get_dict_one(data, description):
+def get_dict_one(data, description) -> dict:
     title = [x[0] for x in description]
     return dict(zip(title, data))
+
+
+def check_not_none(data: dict, ignore: list) -> bool:
+    for k, v in data.items():
+        if k in ignore:
+            continue
+        if isinstance(v, str) and v.strip() == "":
+            return False
+    return True
 
 
 def event_responses_200():
